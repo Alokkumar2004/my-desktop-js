bdfacb3f2340166041c9c51242877e08a928e845
Alok ku. dash <dashalok003@gmail.com>
2025-09-09 08:09:32 +0000

    conversion of datatype

---
+++ startfrombasics/DATATYPE_02.js
@@ -0,0 +1,27 @@
+"use strict";// treat all js code as newer version
+
+//alert(3 + 3)// we are using nodejs, not browser
+
+console.log(3+ 3
+) //code readability is very hard
+
+console.log("alok")
+
+let name="hitesh"
+let age=18
+let isLoggedIn= false
+
+
+
+//number => 2 to power 53
+//bigInt
+//string => using in "" format
+// boolean => using as true and false
+//null => standalone value its a special type
+//undefind =>
+//symbol=> using for unique 
+
+
+//object
+
+console.log(typeof"alok");
---
+++ startfrombasics/veriable_01.js
@@ -0,0 +1,21 @@
+const accountId =144553
+let accountEmail= "alok@gmail.com"
+var accountPassword="12345"
+accountCity="ODISHA"
+
+//accountId=2// not allowed
+
+
+accountEmail="al@al.com"
+accountPassword="2323232"
+accountCity="odisha"
+
+/*prefer not to use var
+ because of issue in block scope and functional scope
+ we can use let instate of var 
+ */
+
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
