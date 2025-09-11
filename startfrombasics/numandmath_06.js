5b67cc5b183fa9a55e412b6488c4fd015c1eda9a
Alok ku. dash <dashalok003@gmail.com>
2025-09-11 09:20:52 +0000

    done that

--- startfrombasics/numandmath_06.js
+++ startfrombasics/numandmath_06.js
@@ -24,3 +24,28 @@ console.log(otherNumber.toPrecision(3));
 
   console.log(Math);
   console.log(Math.abs(-4));
+  //The Math.abs() static method returns the absolute value of a number.
+  console.log(Math.round(4.6));
+  //The Math.round() static method returns the value of a number rounded to the nearest integer
+  console.log(Math.ceil(4.2));
+  //The Math.ceil() static method always rounds up and returns the smallest integer greater than or equal to a given number.
+  console.log(Math.floor(4.4));
+  //The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
+  console.log(Math.min(4, 6, 7 ,3));
+  //The Math.min() static method returns the smallest of the numbers given as input parameters, or Infinity if there are no parameters.
+
+  console.log(Math.random());
+  /*The Math.random() static method returns a floating-point, pseudo-random number that's greater than or 
+  equal to 0 and less than 1, with approximately uniform distribution over that range
+   — which you can then scale to your desired range.
+    The implementation selects the initial seed to the random number generation algorithm;
+     it cannot be chosen or reset by the user.*/
+    console.log(Math.floor(Math.random()*10)+1); 
+    //for lower intiger value
+    const min =10
+    const max = 20
+
+     console.log(Math.floor(Math.random()* (max - min +1))+ min); 
+     // to find a whole number between two value
+
+
